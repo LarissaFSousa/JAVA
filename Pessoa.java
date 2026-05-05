17bdb6cf45cb26d89543d46e1c846ffbada34436
Lara.sz6 <larissafeitosasousa524@gmail.com>
2026-05-05 17:04:45 -0300

    Primeiro commit

---
+++ Pessoa.java
@@ -0,0 +1,41 @@
+package LELE;
+
+//Construtor
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String cpf;
+
+    public Pessoa(String nome, int idade, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+//Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+//Setters
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
